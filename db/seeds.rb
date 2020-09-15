04a191f1534a964d5d03e8be2f0441a9bf29bcda
sophie alexandre <sophie.alec@gmail.com>
2020-09-15 15:21:16 -0400

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,10 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+
+Creation.create(nom: "Robe", description: "Stylée", difficulte: 1, created_at: "2020-09-15 17:21:07", updated_at: "2020-09-15 17:21:07", photo1: nil, photo2: nil, photo3: nil, photo4: nil)
+
+Creation.create(nom: "Jupe", description: "Classe", difficulte: 3, created_at: "2020-09-15 17:21:07", updated_at: "2020-09-15 17:21:07", photo1: nil, photo2: nil, photo3: nil, photo4: nil)
+
+Creation.create(nom: "Pantalon", description: "Décontracté", difficulte: 2, created_at: "2020-09-15 17:21:07", updated_at: "2020-09-15 17:21:07", photo1: nil, photo2: nil, photo3: nil, photo4: nil)
+
+Creation.create(nom: "Combi", description: "Stylée", difficulte: 1, created_at: "2020-09-15 17:21:07", updated_at: "2020-09-15 17:21:07", photo1: nil, photo2: nil, photo3: nil, photo4: nil)
+
